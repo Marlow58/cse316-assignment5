b294ed08d06083f01f9bd9e44ac25831016db8b3
Seongwoo Hong <silton0506@gmail.com>
2024-10-23 13:55:26 +0900

    Finally got to make the sidebar full height (height:100vh was the answer)

--- src/DropdownOwner.jsx
+++ src/DropdownOwner.jsx
@@ -28,7 +28,7 @@ const DropdownOwner = () => {
   const userName = "Producer.P"
 //   const userIcon = "./assets/ikgefu-1024x1024.jpg"
     return (
-  <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: "280px", marginBottom: "auto"}}>
+  <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: "280px", height: "100vh"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"/></svg>
         <span className="fs-4">User</span>
